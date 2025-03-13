78681b8fa88b2ad0c946ec7730c4acf5df3d286d
Gabriel <gabriel.hideaquy@gmail.com>
2025-03-13 12:41:07 -0300

    Update testeEx2.c

--- testeEx2.c
+++ testeEx2.c
@@ -3,18 +3,21 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+char letraNumero(char letNum[50]);
+
 int main() {
 
     char letNum[50];
     char num[10] = "";
     int index = 0;
 
-    scanf("%s", letNum);
+    //scanf("%s", letNum);
+    letraNumero(letNum);
     
     for (int i = 0; i < strlen(letNum); i++) {
-        //printf("i: %d\n", i);
+        //printf("i[%d] = %c\n", i, letNum[i]);
         if (isdigit(letNum[i])) {
-            num[index] += letNum[i];
+            num[index] = letNum[i];
             //printf("i[%d] = %c\n", i, letNum[i]);
             //printf("index antes: %d\n", index);
             //printf("num[%d] = %c\n", index, num[index]);
@@ -22,18 +25,34 @@ int main() {
             //printf("index depois: %d\n", index);
             //printf("num: %s\n", num);
             //printf("num[%d] = %c\n", index, num[index]);
-            if (!isdigit(letNum[i + 1])) {
+            while (!isalpha(letNum[i - 1]) || isdigit(letNum[0])) {
+                printf("\nDigite de acordo com o formato correto.\nEx: LetraNumero-LetraNumero\nOu digite '/sair' para sair.\n");
+                letraNumero(letNum);
+                i = 0;
+                num[0] = '\0';
+                index = 0;
+                break;
+            }
+            if (!isdigit(letNum[i + 1]) || letNum[i + 1] == '\0') {
                 //printf("Entrando no if...\n");
                 //printf("num[%d] = %c\n", index, num[index]);
                 //printf("index if: %d\n", index);
                 int numero = atoi(num);
-                //printf("i: %d\n", i);
+                //printf("num: %d", numero);
+                //printf("i[%d] = %c\n", i, letNum[i]);
                 //printf("index: %d\n", index);
+
                 for (int l = 0; l < numero; l++) {
                     printf("%c", letNum[i - (index)]);
                 }
-                num[index] = '\0';
+
+                for (int k = 0; k < index; k++) {
+                    num[k] = '\0';
+                }
+                //num[0] = '\0';
                 index = 0;
+                //printf("\nindex dps do print: %d", index);
+                //printf("num: %d", num);
 
                 //printf("index depois depois: %d\n", index);
             }
@@ -45,20 +64,31 @@ int main() {
                 for (int j = 0; j < numero; j++) {
                     printf("%c", letNum[i - (index - 1)]);
                 }
-                num[index] = '\0';
-                index = 0;
-            } else {
-                int numero = letNum[i] - '0';
-                for (int k = 0; k < numero; k++) {
-                    printf("%c", letNum[i - 1]);
+
+                for (int k = 0; k < index; k++) {
+                    num[k] = '\0';
                 }
-                num[index] = '\0';
+                //num[0] = '\0';
                 index = 0;
-            }
+            } //else {
+                //if ()
+                //printf("\ni[%d] = %c", i, letNum[i]);
+                
+            //}
             //
 
         }
     }
 
     return 0;
+}
+
+char letraNumero(char letNum[50]) {
+    scanf("%s", letNum);
+    for (int i = 0; i < strlen(letNum); i++) {
+        letNum[i] = tolower(letNum[i]);
+    }
+    if (letNum == "/sair") {
+        exit(0);
+    }
 }
