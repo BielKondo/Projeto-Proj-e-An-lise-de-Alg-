943d2c9e622bba221568fdda39a8afab4a080cdb
Gabriel <gabriel.hideaquy@gmail.com>
2025-03-12 23:38:26 -0300

    Teste do Ex2
    Criei outro arquivo para testar o Ex2. Estava muito preso ao que havia escrito e pensado antes.

---
+++ testeEx2.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main() {
+
+    char letNum[50];
+    char num[10] = "";
+    int index = 0;
+
+    scanf("%s", letNum);
+    
+    for (int i = 0; i < strlen(letNum); i++) {
+        //printf("i: %d\n", i);
+        if (isdigit(letNum[i])) {
+            num[index] += letNum[i];
+            //printf("i[%d] = %c\n", i, letNum[i]);
+            //printf("index antes: %d\n", index);
+            //printf("num[%d] = %c\n", index, num[index]);
+            index++;
+            //printf("index depois: %d\n", index);
+            //printf("num: %s\n", num);
+            //printf("num[%d] = %c\n", index, num[index]);
+            if (!isdigit(letNum[i + 1])) {
+                //printf("Entrando no if...\n");
+                //printf("num[%d] = %c\n", index, num[index]);
+                //printf("index if: %d\n", index);
+                int numero = atoi(num);
+                //printf("i: %d\n", i);
+                //printf("index: %d\n", index);
+                for (int l = 0; l < numero; l++) {
+                    printf("%c", letNum[i - (index)]);
+                }
+                num[index] = '\0';
+                index = 0;
+
+                //printf("index depois depois: %d\n", index);
+            }
+        } else {
+            //printf("index (else): %d\n", index);
+            //printf("ELSE: i[%d] = %c\n", i, letNum[i]);
+            if (index > 0) {
+                int numero = atoi(num);
+                for (int j = 0; j < numero; j++) {
+                    printf("%c", letNum[i - (index - 1)]);
+                }
+                num[index] = '\0';
+                index = 0;
+            } else {
+                int numero = letNum[i] - '0';
+                for (int k = 0; k < numero; k++) {
+                    printf("%c", letNum[i - 1]);
+                }
+                num[index] = '\0';
+                index = 0;
+            }
+            //
+
+        }
+    }
+
+    return 0;
+}
